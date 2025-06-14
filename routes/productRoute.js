e1f241af95f77e56e77d9b5cf68c654ab268513c
subupkrl <139966058+subupkrl@users.noreply.github.com>
2025-06-14 14:09:22 +0545

    little bit modifications done

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,12 +4,12 @@ const { requireAdmin } = require("../controllers/authController");
 
 const router = express.Router();
 const {upload,handleMulterError} = require("../middleware/fileupload");
-const { productValidation, validation, categoryValidation } = require("../validation/validator");
+const { productValidation, validation} = require("../validation/validator");
 
 router.post("/postproduct",upload.single('product_image'),productValidation,validation,postProduct)
 router.get("/productlist",productList)
 router.get("/productdetails/:id",productDetails)
-router.put("/updateproduct/:id",categoryValidation,validation,updateProduct)
+router.put("/updateproduct/:id",upload.single('product_image'),productValidation,validation,requireAdmin,updateProduct)
 router.delete("/deleteproduct/:id",requireAdmin,deleteProduct)
 router.use(handleMulterError)
 
